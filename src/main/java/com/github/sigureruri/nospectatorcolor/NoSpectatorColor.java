30b54c1d4da4def0af52598020b457afc5901185
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2019-09-26 16:16:21 +0900

    first commit

---
+++ src/main/java/com/github/sigureruri/nospectatorcolor/NoSpectatorColor.java
@@ -0,0 +1,16 @@
+package com.github.sigureruri.nospectatorcolor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NoSpectatorColor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
